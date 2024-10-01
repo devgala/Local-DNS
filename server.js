d850cad06f560bb9311e63a2b41860c5fc22f1f6
Dev Gala <114738592+devgala@users.noreply.github.com>
2024-10-01 22:36:23 +0530

    da y2

--- server.js
+++ server.js
@@ -8,52 +8,57 @@ const server = dgram.createSocket("udp4");
  */
 
 const db = {
-  "go-google": {
-    data: 'google.com'
+  "devgala.facebook.com": {
+    data: 'www.facebook.org'
+  },
+  "devgala.google.com": {
+    data: 'www.google.com'
+  },
+  "devgala.wikipedia.org": {
+    data: 'www.wikipedia.org'
+  },
+  "devgala.whatsapp.com": {
+    data: 'www.whatsapp.com'
   },
 };
-var port = 1025;
-var query_adderss
+var port = {}
+var query_adderss = {}
 var questions = {}
 server.on("message", (msg, rinfo) => {
-    
   const incomingReq = dnsPacket.decode(msg);
-  console.log('Message From :',rinfo.address,rinfo.port,'Query: ',incomingReq.questions[0].name)
-  console.log(incomingReq,rinfo);
+  console.log(incomingReq, rinfo);
 
-  if(incomingReq.questions[0].name=='go-google'){
-    
-     port = rinfo.port
-     questions = incomingReq.questions
-     query_adderss = rinfo.address
+  if (incomingReq.questions[0].name.startsWith('devgala.') && incomingReq.type==='query') {
+
+    port[incomingReq.id] = rinfo.port
+    questions[incomingReq.id] = JSON.parse(JSON.stringify(incomingReq.questions))
+    query_adderss[incomingReq.id] = rinfo.address
     const resolvedName = db[incomingReq.questions[0].name];
-    console.log('Message From :',rinfo.address,rinfo.port,'Query: ',incomingReq.questions[0].name,resolvedName)
-    const query = dnsPacket.encode({
-        type:'query',
-        id: incomingReq.id,
-        flags : incomingReq.flags,
-        questions: [{
-            type: incomingReq.questions[0].type,
-            class: incomingReq.questions[0].class,
-            name:resolvedName.data
-        }]
-    })
-
-    server.send(query,53,'4.2.2.2',(err,bytes)=>console.log('error' ,bytes, err))
-  }else{
-    const ans = incomingReq
-    ans.questions = questions
-    server.send(dnsPacket.encode(ans), port,query_adderss,(err,bytes)=>console.log('Return error ',bytes,err));
+    incomingReq.questions[0].name = resolvedName.data
+    server.send(dnsPacket.encode(incomingReq), 53, '1.1.1.1', (err, bytes) => console.log('Send Intercept error', bytes, err))
+  } else if (incomingReq.type === 'query') {
+    questions[incomingReq.id] = incomingReq.questions
+    query_adderss[incomingReq.id] = rinfo.address
+    port[incomingReq.id] = rinfo.port
+    server.send(dnsPacket.encode(incomingReq), 53, '1.1.1.1', (err, bytes) => console.log('Send non-intercept error', bytes, err))
+  } else {
+    const resp = incomingReq
+    resp.questions = questions[incomingReq.id]
+    if(resp.answers  && resp.answers.length > 0 && resp.answers[0].name)
+    resp.answers[0].name = questions[incomingReq.id][0].name
+    server.send(dnsPacket.encode(resp), port[incomingReq.id], query_adderss[incomingReq.id], (err, bytes) => console.log('Send ans error ', bytes, err));
   }
+
+
 
 
 
 });
 
 server.on('listening', () => {
-    const address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
-  });
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
 
-server.on('error',(err)=>console.log('Error ',err ))
+server.on('error', (err) => console.log('Error ', err))
 server.bind(53, () => console.log("DNS Server is running on port 53"));
